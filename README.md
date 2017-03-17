97777a8d45f4e9549e7d67ff1c2e17c4695264c9
robneal <robneal28@gmail.com>
2017-03-16 23:23:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project Title 
+
+---
+
+### Author(s): Robert Neal 
+
+### Description: Thsi is a simple template that can be used to start a project.  
+
+#### Project Status : In Progress
+
+#### GitHub Pages Link - https://robneal.github.io/
---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.nojs').hide(); 
+
+  $(function() {
+    console.log("js is (t)werking");
+    $('#typedJS').typed({
+      strings: [
+      "Hey there stanger!",
+      //"I'm robert.",
+      "I'm still building this site ...",
+      "but stalk me below.",
+      "Robert Neal"
+      ],
+      typeSpeed: 60,
+      backSpeed: 30,
+      backDelay: 1400,
+      loop: true,
+    });
+  }); 
+
+});
